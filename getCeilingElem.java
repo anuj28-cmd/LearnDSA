b7970cca0224b298f5824c5810249a8e0eca2bd4
anuj28-cmd <153478127+anuj28-cmd@users.noreply.github.com>
2023-12-12 11:05:46 +0530

    changes in code

--- getCeilingElem.java
+++ getCeilingElem.java
@@ -2,6 +2,7 @@ package LearnDSA;
 import java.util.Scanner;
 
 public class getCeilingElem{
+    // using linnear search :
     public static int search(int[]arr,int target){
         
         for(int i =0;i<arr.length;i++){
@@ -20,6 +21,26 @@ public class getCeilingElem{
     }
      return 0;
     }
+//     // Using binary search
+    public static int B_search(int[]arr,int x){
+       int start = 0;
+       int end = arr.length -1 ;
+       int mid=0;
+       while (start!=end) {
+         mid = start + (end-start)/2;
+        if (x == arr[mid]){
+            return mid;
+        }
+        else if (x < arr[mid]){
+            end=mid-1;
+        return 0;
+       }
+       else{
+        start = mid +1;     
+    }
+}
+    return mid;
+}
     public static void main(String[] args) {
         
     
@@ -27,6 +48,7 @@ public class getCeilingElem{
     Scanner sc =new Scanner(System.in);
     int target = sc.nextInt();
     search(arr, target);
+    B_search(arr, target);
     sc.close();
    }
 }
