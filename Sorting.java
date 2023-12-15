66da779823fce44327924e27f55c9df7e0b07941
anuj28-cmd <153478127+anuj28-cmd@users.noreply.github.com>
2023-12-16 00:52:01 +0530

    added selection sort program in Sorting File

--- Sorting.java
+++ Sorting.java
@@ -1,7 +1,14 @@
 public class Sorting {
-    public static void main(String[] args) {
-        int[] array = new int[]{5, 3, 8, 10, -2};
-        Bubble_sort(array);
+   
+    public static void print(int[]arr){
+         System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if(i != arr.length -1 ){
+            System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
     public static void Bubble_sort(int[]arr){
         for (int i = 0; i < arr.length -1 ; i++) {
@@ -13,17 +20,32 @@ public class Sorting {
             arr[j] = temp;
          }
        } 
+       print(arr);
     }
-        System.out.print("[");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-            if(i != arr.length -1 ){
-            System.out.print(", ");
-            }
+}
+    public static void Selection_Sort(int[] arr){
+        int n = arr.length;
+       for (int i = 0; i < n-1; i++) {
+            int min= i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j]<arr[min]){
+                min = j;
         }
-        System.out.println("]");
-    
-    
+    }
+    int temp = arr[min];
+    arr[min] = arr[i];
+    arr[i] = temp;
+}
+print(arr);
+    }
+
+ public static void main(String[] args) {
+        int[] array = new int[]{5, 3, 8, 10, -2};
+        // Bubble_sort(array);
+        Selection_Sort(array);
+
+            
+            
     }
 }
 
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+else{
+                int min=0;
+                System.out.println(min);
+            }
